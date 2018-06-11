c13a912befbd73867102f17c9541b2a9237eff92
Matthew Territo <territo-sci@users.noreply.github.com>
2018-06-11 16:48:36 -0600

    Bridging TuioServer: interaction handlers and setters used by TuioPad Sender. Need to figure out how to properly use namespaced C++ classes/objects as parameters in Obj-C headers.

--- FlightController/FlightController/TuioServerBridge.h
+++ FlightController/FlightController/TuioServerBridge.h
@@ -7,7 +7,38 @@
 //
 #import <Foundation/Foundation.h>
 
+// MARK: Forward declarations
+@class TuioCursor;
+@class TuioBlob;
+@class TuioTime;
+
 @interface TuioServer: NSObject
-- (void)testFunction;
+// MARK: Initializaters
+- (id)initWithHost:(NSString *)host port:(int)port;
+- (void)initialize:(NSString *)host port:(int)port;
+- (void)setSourceName:(NSString *)name host:(NSString *)host;
+
+// MARK: Interaction Handlers
+- (void)initFrame:(TuioTime *)time;
+
+- (void)addTuioCursor:(float)x y:(float)y;
+- (void)addTuioBlob:(float)x y:(float)y a:(float)a w:(float)w h:(float)h f:(float)f;
+
+- (void)removeTuioCursor:(TuioCursor *)cursor;
+- (void)removeTuioBlob:(TuioBlob *)blob;
+
+- (void)updateTuioCursor:(TuioCursor *)cursor x:(float)x y:(float)y;
+- (void)updateTuioBlob:(TuioBlob *)blob x:(float)x y:(float)y a:(float)a w:(float)w h:(float)h f:(float)f;
+
+- (void)stopUntouchedMovingCursors;
+- (void)stopUntouchedMovingBlobs;
+
+- (void)commitFrame;
+
+// MARK: Setters
+- (void)enableObjectProfile:(BOOL) flag;
+- (void)enableBlobProfile:(BOOL) flag;
+
+
 @end
 
--- FlightController/FlightController/TuioServerBridge.mm
+++ FlightController/FlightController/TuioServerBridge.mm
@@ -13,20 +13,82 @@
 
 @implementation TuioServer
 
-TUIO::TuioServer *myObject;
-
+// MARK: Initializers
 - (id)init {
     self = [super init];
     if (self) {
         // init variables
-        myObject = new TUIO::TuioServer();
+        obj_ = new TUIO::TuioServer();
     }
     return self;
 }
 
-- (void)testFunction
-{
-    printf("Update interval: %d\n", myObject->getUpdateInterval());
+- (id)initWithHost:(const NSString *) host port:(int)port {
+    self = [super init];
+    if (self) {
+        // init variables
+        obj_ = new TUIO::TuioServer([host UTF8String], port);
+    }
+    return self;
 }
+
+- (void)initialize:(NSString *)host port:(int)port { }
+
+- (void)setSourceName:(NSString *)name host:(NSString *)host {
+    obj_->setSourceName([name UTF8String], [host UTF8String]);
+}
+
+// MARK: Interaction Handlers
+- (void)initFrame:(TuioTime *)time {
+    obj_->initFrame(*((__bridge TUIO::TuioTime *) time));
+}
+
+- (void)addTuioCursor:(float)x y:(float)y {
+    obj_->addTuioCursor(x, y);
+}
+
+- (void)addTuioBlob:(float)x y:(float)y a:(float)a w:(float)w h:(float)h f:(float)f {
+    obj_->addTuioBlob(x, y, a, w, h, f);
+}
+
+- (void)removeTuioCursor:(TuioCursor *)cursor {
+    obj_->removeTuioCursor((__bridge TUIO::TuioCursor *) cursor);
+}
+
+- (void)removeTuioBlob:(TuioBlob *)blob {
+    obj_->removeTuioBlob((__bridge TUIO::TuioBlob *) blob);
+}
+
+- (void)updateTuioCursor:(TuioCursor *)cursor x:(float)x y:(float)y {
+    obj_->updateTuioCursor((__bridge TUIO::TuioCursor *) cursor, x, y);
+}
+
+- (void)updateTuioBlob:(TuioBlob *)blob x:(float)x y:(float)y a:(float)a w:(float)w h:(float)h f:(float)f {
+    obj_->updateTuioBlob((__bridge TUIO::TuioBlob *) blob, x, y, a, w, h, f);
+}
+
+- (void)stopUntouchedMovingCursors {
+    obj_->stopUntouchedMovingCursors();
+}
+
+- (void)stopUntouchedMovingBlobs {
+    obj_->stopUntouchedMovingBlobs();
+}
+
+- (void)commitFrame {
+    obj_->commitFrame();
+}
+
+// MARK: Set Attributes
+- (void)enableObjectProfile:(BOOL)flag {
+    obj_->enableObjectProfile(flag);
+}
+
+- (void)enableBlobProfile:(BOOL)flag {
+    obj_->enableBlobProfile(flag);
+}
+
+// MARK: Members
+TUIO::TuioServer *obj_;
 
 @end
